01942f93adf7ef07d23394bb8e07b5456f0eaf6d
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2023-10-09 09:38:00 +0530

    adding files

---
+++ src/Day1_100daysOfCode/Introduction.java
@@ -0,0 +1,17 @@
+/*package Day1_100daysOfCode;*/
+
+/*//Topic 1:Introduction to Java
+public class Introduction {
+}
+
+    Topic of #Day 1 :Fundamentals of Java
+
+        ✅ Java was Introduced in 1991.
+        ✔ It is High Level Learning
+        ✔ It is an Object Oriented Programming Language
+        ✔It is an Portable and Platform Independent(WORA:write once run anywhere)
+        ✔Jvm will convert byte code into Mll(Machine Level Language) internally make use of interpreter and take use of Java Byte code and Its platform independent.
+        ✔Java Compiler will convert the Java Program or Java Source Code into
+        Byte code
+        ✔Java Program -->JavaC(Java Compiler)-->Byte Code--->classfile(FirstProgram.class).
+        ✔Machine Level Language is always a PlatForm Dependentadd*/
---
+++ src/Day2_100daysOfCode/FirstJava.java
@@ -0,0 +1,12 @@
+package Day2_100daysOfCode;
+
+//Topic:Main Method
+//Date: 09 Oct 2023
+
+public class FirstJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World,First Program");
+
+    }
+}
