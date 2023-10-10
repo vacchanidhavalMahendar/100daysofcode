f69e0071bc8c18a3e42e2512d211d51c924f521b
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2023-10-10 09:37:14 +0530

    adding files

---
+++ src/Day3_100daysOfCode/DataType_Example_2.java
@@ -0,0 +1,45 @@
+package Day3_100daysOfCode;
+
+//Topic2 :Data Types
+//Number Data: byte,short,int,long
+//Character Data Type: To Represent characters
+//boolean:To represent Logical values(true or false)
+//Float data type:To Represent real numbers(float double)
+
+public class DataType_Example_2 {
+public static void main(String[] args){
+
+    //Number Data
+
+    int b=10;
+    byte result=20;
+    short b1=2;
+    long l=96766355353L;
+
+    //character data
+    char c='c';
+
+    //boolean
+    boolean ex=true;
+    boolean b2=false;
+
+    //float data
+
+    float f=2.0f;
+    double d=3.02d;
+
+    System.out.println(b);
+    System.out.println(result);
+    System.out.println(b1);
+    System.out.println(l);
+    System.out.println(c);
+    System.out.println(ex);
+    System.out.println(b2);
+    System.out.println(f);
+    System.out.println(d);
+
+
+
+
+}
+}
---
+++ src/Day3_100daysOfCode/OopIntroduction_Car_1.java
@@ -0,0 +1,31 @@
+/*
+package Day3_100daysOfCode;
+
+//Topic 1:Object Oriented Programming Basic Introduction
+//What is Object: It is an real time instances OR Entity
+//Object has two parts:What it has and What it does
+
+public class OopIntroduction_Car_1 {
+public static void main(String[] args){
+
+    //Has Part of an Object  is represented as "Variables"
+    String brandName;
+    int carNo;
+
+    //Does Part of an Object is Represented as "Method"
+
+    public void  move(){
+        //Logic of Vehicle
+        System.out.println("Move the vehicle");
+    }
+
+
+
+
+
+
+
+
+}
+}
+*/
