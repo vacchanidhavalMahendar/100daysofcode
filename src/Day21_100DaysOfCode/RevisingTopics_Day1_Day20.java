372468bf2561494ebbaa6dc7d8dcae1a6cb5d5a2
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2023-10-29 18:59:22 +0530

    adding files

---
+++ src/Day21_100DaysOfCode/RevisingTopics_Day1_Day20.java
@@ -0,0 +1,40 @@
+/*
+package Day21_100DaysOfCode;
+
+public class RevisingTopics_Day1_Day20 {
+}
+    Revised Topics,Which are Covered from Day 1 to Day 20..
+
+
+
+        ✔ Fundamentals Of Programming.
+
+        ✔Java program and Main method in Java.
+
+        ✔ Identifiers, Reserved keyword, Data types in Java.
+
+        ✔Data types and Incrementation , Decrementation operator
+
+        ✔Operators , Conditionals , Switch case in Java.
+
+        ✔Loops and Pattern Programming
+
+        ✔Classes , Objects, JVM data areas
+
+        ✔Methods in Java and Fundamental of stack and heap area
+
+        ✔Method Overloading and Array Introduction.
+
+        ✔Array in Java
+
+        ✔ String with memory map in detail
+
+        ✔String in Java and Mutable String.
+
+        ✔Mutable String and String programming
+
+        ✔ String Buffer and String Builder.
+
+        ✔ Encapsulation Introduction.
+
+*/
