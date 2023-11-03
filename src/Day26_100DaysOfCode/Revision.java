8fb74531e7131bbd9fc5cc3653e3b5371bb4ef09
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2023-11-03 19:53:15 +0530

    adding files

---
+++ src/Day26_100DaysOfCode/Revision.java
@@ -0,0 +1,12 @@
+package Day26_100DaysOfCode;
+
+public class Revision {
+}
+
+ /* Day 6 to Day 10
+
+          Ternary Operator
+          Switch Statement.
+          Loops:for loop,while loop,do while
+class,Objects,JVM,Data Areas.
+        Class Methods in Java and Fundamentals of Stack and Heap Area.*/
